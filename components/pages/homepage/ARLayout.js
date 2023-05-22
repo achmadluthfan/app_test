3712fa6df0d8ecc1690cc33491bbb5affa06257a
Anantadhirya <anantadhirya@gmail.com>
2023-05-22 21:02:43 +0700

    feat : AR Layout page

--- components/pages/homepage/ARLayout.js
+++ components/pages/homepage/ARLayout.js
@@ -1,7 +1,17 @@
 export function ARLayout() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-blue-500 underline">AR</h1>
+      <div className="mx-[3%] h-fit min-h-[250px] lg:mx-[5%]">
+        <div className="mt-[10vh] text-center font-rubik text-2xl font-semibold text-blue-900 duration-300 sm:text-4xl md:text-5xl 2xl:text-6xl">
+          Jelajahi UGM secara <br />
+          Virtual dengan <span className="font-damion">AR</span>
+        </div>
+        <div className="mt-[1vh] text-center font-rubik text-sm duration-300 sm:text-lg md:text-xl 2xl:text-2xl">
+          UGM AR adalah sebuah konsep yang disajikan oleh Panitia PPSMB UGM 2022 dalam bentuk
+          Infographic yang menampilkan informasi-informasi UGM secara keseluruhan.
+        </div>
+        <div className="mb-[20vh] mt-[3vh] h-[50vw] max-h-[56vh] rounded-[6px] bg-white outline outline-[6px] -outline-offset-[5px] outline-blue-500 duration-300" />
+      </div>
     </div>
   )
 }
